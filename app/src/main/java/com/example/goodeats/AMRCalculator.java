761ad072a659e7b988bfd8e086c0994f8b517f8f
bashiruu1 <bashiruu1@gmail.com>
2019-03-30 19:51:02 -0400

    Second commit - working on stage two, to get AMR to be converted into a decent meal plan

--- app/src/main/java/com/example/goodeats/AMRCalculator.java
+++ app/src/main/java/com/example/goodeats/AMRCalculator.java
@@ -2,17 +2,35 @@ package com.example.goodeats;
 
 public class AMRCalculator {
 
-    public int calculateFemalesBMR(int weight, int height, int age) {
-        int BMR = (int) (655 + (4.35 * weight) + (4.7 * height) - (4.7 * age));
-        return BMR;
+    double bmr;
+    double amr;
+    String[] activityLevels = {"Light", "Moderate", "Active", "Very Active"};
+
+
+    public String getActivityLevel(int a) {
+        return activityLevels[a];
+    }
+    public double calculateFemaleBMR(double weight, double height, double age) {
+        bmr = (655 + (4.35 * weight) + (4.7 * height) - (4.7 * age));
+        return bmr;
     }
 
-    public int calculateMaleBMR(int weight, int height, int age) {
-        int BMR = (int) (66 + (6.23 * weight) + (12.7 * height) - (6.8 * age));
-        return BMR;
+    public double calculateMaleBMR(double weight, double height, double age) {
+        bmr = (66 + (6.23 * weight) + (12.7 * height) - (6.8 * age));
+        return bmr;
     }
 
-    public int calculateAMR (int BMR, String activityLevel) {
-        return BMR;
+    public double calculateAMR(double bmr, String activityLevel) {
+        if (activityLevel.equals(activityLevels[0])) {
+            return bmr * 1.375;
+        } else if (activityLevel.equals(activityLevels[1])) {
+            return bmr * 1.55;
+        } else if (activityLevel.equals(activityLevels[2])) {
+            return bmr * 1.725;
+        } else if (activityLevel.equals(activityLevels[3])) {
+            return bmr * 1.9;
+        }
+        return -1.0;
     }
+
 }
--- app/src/main/java/com/example/goodeats/MainActivity.java
+++ app/src/main/java/com/example/goodeats/MainActivity.java
@@ -1,5 +1,6 @@
 package com.example.goodeats;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
@@ -17,12 +18,17 @@ import android.widget.Toast;
 public class MainActivity extends AppCompatActivity {
 
     Button calculateCalories;
+    Button createMealPlan;
     EditText age;
     EditText height;
     EditText weight;
+    Double bmr;
+    Double amr;
     RadioGroup gender;
     RadioButton genderChoice;
     Spinner activityLevel;
+    String currentLevel;
+    String currentGoalLevel;
     Spinner goalLevel;
     TextView calorieView;
     AMRCalculator amrCalculator;
@@ -38,6 +44,7 @@ public class MainActivity extends AppCompatActivity {
         weight = findViewById(R.id.weight);
         gender = findViewById(R.id.gender);
         calorieView = findViewById(R.id.calorie_view);
+        amrCalculator = new AMRCalculator();
 
         activityLevel = findViewById(R.id.activity_level);
         ArrayAdapter<CharSequence> activityLevelAdapter = ArrayAdapter.createFromResource(MainActivity.this, R.array.activities, R.layout.support_simple_spinner_dropdown_item);
@@ -46,7 +53,7 @@ public class MainActivity extends AppCompatActivity {
         activityLevel.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
             @Override
             public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
-                String currentLevel = parent.getItemAtPosition(position).toString();
+                currentLevel = parent.getItemAtPosition(position).toString();
                 Toast.makeText(MainActivity.this, currentLevel, Toast.LENGTH_LONG).show();
             }
 
@@ -63,7 +70,7 @@ public class MainActivity extends AppCompatActivity {
         goalLevel.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
             @Override
             public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
-                String currentGoalLevel = parent.getItemAtPosition(position).toString();
+                currentGoalLevel = parent.getItemAtPosition(position).toString();
                 Toast.makeText(MainActivity.this, currentGoalLevel, Toast.LENGTH_LONG).show();
             }
 
@@ -75,6 +82,29 @@ public class MainActivity extends AppCompatActivity {
 
         calculateCalories = findViewById(R.id.calculate_calorie);
         calculateCalories.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                bmr = amrCalculator.calculateMaleBMR(Double.valueOf(weight.getText().toString()), Double.valueOf(height.getText().toString()), Double.valueOf(age.getText().toString()));
+                if (currentLevel.equals(amrCalculator.getActivityLevel(0))) {
+                    amr = Math.ceil(bmr * 1.375);
+                    Toast.makeText(MainActivity.this, amrCalculator.activityLevels[0], Toast.LENGTH_LONG).show();
+                } else if (currentLevel.equals(amrCalculator.activityLevels[1])) {
+                    amr = Math.ceil(bmr * 1.55);
+                    Toast.makeText(MainActivity.this, amrCalculator.activityLevels[1], Toast.LENGTH_LONG).show();
+                } else if (currentLevel.equals(amrCalculator.activityLevels[2])) {
+                    amr = Math.ceil(bmr * 1.725);
+                    Toast.makeText(MainActivity.this, amrCalculator.activityLevels[2], Toast.LENGTH_LONG).show();
+                } else if (currentLevel.equals(amrCalculator.activityLevels[3])) {
+                    amr = Math.ceil(bmr * 1.9);
+                    Toast.makeText(MainActivity.this, amrCalculator.activityLevels[3], Toast.LENGTH_LONG).show();
+                }
+                calorieView.setText("You need " +String.valueOf(bmr)+ " calories to keep your body functioning at rest and about " + amr +
+                        " calories based of your level of weekly exercise to maintain your weight");
+            }
+        });
+
+        createMealPlan = findViewById(R.id.create_mealplan_button);
+        createMealPlan.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
 
